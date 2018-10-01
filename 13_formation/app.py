0f777b85036aead4d3483d8d5fdc32448908d3ed
tangym27 <mtang1@stuy.edu>
2018-10-01 12:33:26 -0400

    session stuff

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -3,29 +3,54 @@
 #K13: Echo Echo Echo . . .
 #2018-09-27
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 
 app = Flask (__name__) #create instance of class FLask
 
-@app.route("/") #assign fxn to route
-def hello_world():
-    return render_template('home.html')
+session["username"]= "ultimate"
+session["password"]= "frisbee"
+
+
+@app.route("/auth", methods=['POST'])
+def auth():
+    print url_for('disp_loginpage')
+    print url_for("auth")
+    return redirect(url_for("disp_login")
 
 @app.route("/intro")
 def intro():
     return("Hi. My name is Michelle. wooo. <a href=\"/\"> Hi </a>" )
 
-@app.route("/auth")
+@app.route("/auth" ,methods=["GET"])
 def authenticate():
-    print(app)
+    user = request.form['username']
+   
     print(request)
-    print(request.args)
-    return render_template('user.html',
+    print(request.args['username'])
+    print(request.args['password'])
+    if ((request.args['username'] == session["username"]) and (request.args['password'] == session["password"])):
+        return render_template('user.html',
                            user = request.args['username'],
                            method = request.method)
+    else:
+        return render_template('occupations.html')
     
 
 if __name__ == "__main__": #will only run if it recognizes this a main (not outside source)   
     app.debug = True
     app.run()
 
+@app.route('/')    
+def index():
+   if ('username' in session):
+    return redirect(url_for('authenticate'))
+    else: redirect(url_for('login'))
+       username = session['username']
+       return 'Logged in as ' + username + '<br>' + \
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   if request.method == 'POST':
+      session['username'] = request.form['username']
+      return redirect(url_for('index'))
+  
