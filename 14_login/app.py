7fb85e29bace0ff2921baa41726683a458db79f4
Michelle Tang <mtang1@stuy.edu>
2018-10-02 01:34:41 -0400

    finish hw

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,39 +1,52 @@
-#Michelle Tang
+#I've Had Ahnaf, I'm Tangry: Michelle Tang, Ahnaf Kazi
 #SoftDev1 pd6
-#K13: Echo Echo Echo . . .
-#2018-09-27
+#K#14 -- Do I Know You?    
+#2018-10-02
 
 from flask import Flask, render_template, request, session, url_for, redirect
 import os
 app = Flask (__name__) #create instance of class FLask
-app.secret_key = os.urandom(32)
+app.secret_key = os.urandom(32) 
 
 USER = "ultimate"
 PASS = "frisbee"
 
+#root route!
 @app.route("/", methods = ["POST", "GET"])
 def intro():
+    # if user has already been logged in, automatically go to user page
     if("username" in session and session.get('username') == USER):
       print("still here")
       return render_template("user.html", username = USER)
+    # else bring person to login page
     return render_template("home.html")
 
     
-    
+#login page    
 @app.route("/login", methods=["POST", "GET"])
 def login():
+  #checks if username and password are correct 
   if request.form['username'] == USER and request.form['password'] == PASS:
+    # if it is, establish session & go to user page
     session['username'] = request.form['username'] 
+    print("YOU SHOULD ONLY SEE THIS IS USER AND PASS IS CORRECT")
     return render_template("user.html", username = USER)
+  # if it is not, return appropriate error
+  elif request.form['username'] != USER:
+    return render_template('error.html',error = "username")
   else:
-    return render_template('occupations.html')
+    return render_template('error.html',error = "password")
 
+# logout page
 @app.route("/logout", methods=["POST"])
 def logout():
+    #if button is pressed to logout...
     if(request.form["logout"] == "logout"):
-        session.pop("username", None) #removes username from session, logging out
-        return render_template("home.html")#returns to login page
+        #remove session and go back to login page
+        session.pop("username", None) 
+        return render_template("home.html")
 
-#if __name__ == "__main__": #will only run if it recognizes this a main (not outside source)   
-app.debug = True
-app.run()
+
+if __name__ == "__main__": #will only run if it recognizes this a main (not outside source)   
+  app.debug = True
+  app.run()
