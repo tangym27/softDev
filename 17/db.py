f2b34d4fbc552f07e493d1360279f2e006f9a6dd
Michelle Tang <mtang1@stuy.edu>
2018-10-08 19:22:02 -0400

    added hw17

--- 17/db.py
+++ 17/db.py
@@ -1,12 +1,12 @@
-#TANGerine -- Stefan Tan & Michelle Tang
-#SoftDev1 pd6
-#K16: No Trouble
-#2018-10-04
+#to be decided-Michelle Tang && Britni Canale
+#SoftDev1 pd 6
+#K17: Average
+#10/7/18
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
-DB_FILE="stuStuff.db" #it will create this
+DB_FILE="database.db" #it will create this
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
@@ -25,9 +25,9 @@ with open("data/courses.csv","r") as csvFile: # use with to open/close file
         code = line["code"]
         mark = line["mark"]
         idNum = line["id"]
-        command = "INSERT INTO courses VALUES('" + code + "', " + mark + ", " + idNum + ")"
-        command2="SELECT code FROM courses;"
-        print (c.execute(command2))
+        command2 = "INSERT INTO courses VALUES('" + code + "', " + mark + ", " + idNum + ")"
+       # command2="SELECT code FROM courses;"
+        c.execute(command2)
 
 # creates and execute a table with headers name, age, and id
 create1 = "CREATE TABLE peeps (name TEXT, age INTEGER, id INTEGER);"
