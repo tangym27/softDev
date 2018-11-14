747856408aeeb50aba4166e29b0f0283f56401ec
Michelle Tang <mtang1@stuy.edu>
2018-11-14 17:53:48 -0500

    boop

--- 24_rest/rest.py
+++ 24_rest/rest.py
@@ -1,4 +1,4 @@
-# Michelle tang
+# Michelle Tang
 # SoftDev1 pd6
 # K24 -- A RESTful Journey Skyward
 # 2018-11-13
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    lnk = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=ggxPACCj6D8IeIEilGt5Cs0oGyx7GKeeAXYqmdPP")
+    lnk = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=Qyrxovb3AXTeMsth93T4hVnmzDcSGQDZnHxsr77i")
     data = lnk.read()
     nasa = json.loads(data) #creates a dictionary
     pic = nasa["url"]
