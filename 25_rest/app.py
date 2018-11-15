6345f619c2b845c4f2cb3728d1a098599a9cf33a
Michelle Tang <mtang1@stuy.edu>
2018-11-14 20:48:58 -0500

    i cant believe i started this three hours ago im so tired help me please

---
+++ 25_rest/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from yelpapi import YelpAPI
+
+
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    lnk = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=Qyrxovb3AXTeMsth93T4hVnmzDcSGQDZnHxsr77i")
+    data = lnk.read()
+    nasa = json.loads(data) #creates a dictionary
+    pic = nasa["url"]
+    return render_template("home.html", pic = pic)
+
+@app.route('/api', methods=['GET'])
+def api():
+    yelp_api = YelpAPI("92b7FDwkewHE9-5Ii9ig1hrlYWRtuzGWzaCTSRialTUvjhx7IpvJvRI2KP2T71WiWxlc8eu4ASfYfdiQbPXAkKQvaQyIypoI9j9DXeqecgrgw78PM2CXiJe_JKrsW3Yx")
+    # args="?"
+    # search_results = yelp_api.search_query(args)
+    #print("10")
+    response = yelp_api.phone_search_query(phone='+17189966288')
+    #
+    # print(response)
+    # print("\n\n\n\n\n\n")
+    # print (response['businesses'][0]['name'])
+
+    return render_template('api.html', business = response)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
